c5facb2d8073038b79ea63c049746492c4546359
Minh Tran <93815818+Conabot@users.noreply.github.com>
2023-05-31 14:01:27 -0700

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,30 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+function About(user) {
+ // console.log(bio)
+ /* const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>
+      } */
+
+ const unKnowBio= user.bio;
+ //console.log(unKnowBio.length)
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {unKnowBio && unKnowBio.length > 0 && 
+      <p>{unKnowBio}</p>
+      }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      
+      <Links github={user.github} linkedin={user.linkedin}/>
     </div>
   );
 }
